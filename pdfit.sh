ad08c06abc030381ad87a29f85a4ee2491ab43bf
Marcus Catt <mc@marcuscatt.co.uk>
2016-07-13 15:56:08 +0100

    working set for gb sky offers

--- pdfit.sh
+++ pdfit.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-for file in $( ls reports/site/cucumber-reports/*.html ) do
-	echo $file
+flist=""
+for f in $( ls reports/site/cucumber-reports/*.html | sort ); do
+	echo "$file"
+	bname=`basename $f`
+	echo "$bname"
+	flist="$flist $bname"
 done
+echo "result is : $flist"
+cd reports/site/cucumber-reports
+st=`wkhtmltopdf $flist cucumer-report.pdf`
+exit 0
 
