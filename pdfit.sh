7470f48660e396e4cccbdb865863bc3fb515ff1a
Marcus Catt <mc@marcuscatt.co.uk>
2016-07-14 15:46:18 +0100

    basically working now

--- pdfit.sh
+++ pdfit.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 flist=""
+# improvment - path should be an env or param
 for f in $( ls reports/site/cucumber-reports/*.html | sort ); do
-	echo "$file"
 	bname=`basename $f`
-	echo "$bname"
 	flist="$flist $bname"
 done
-echo "result is : $flist"
+#echo "result is : $flist"
+date=`date +%Y%m%d_%H%M%S`
 cd reports/site/cucumber-reports
-st=`wkhtmltopdf $flist cucumer-report.pdf`
+st=`wkhtmltopdf $flist cucumer-report-${date}.pdf`
 exit 0
 
