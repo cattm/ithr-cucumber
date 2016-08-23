4fbc01124dce0d148c8c3db524092d6108d78d1a
Marcus Catt <mc@marcuscatt.co.uk>
2016-08-23 13:19:35 +0100

    moved test feature file outside of project [Issue4_offers]

--- src/test/java/com/ithr/ppe/test/cucumber/CucumberRunner.java
+++ src/test/java/com/ithr/ppe/test/cucumber/CucumberRunner.java
@@ -9,9 +9,9 @@ import cucumber.api.junit.Cucumber;
 		plugin = { "html:reports/cucumber-html-report",
 				   "json:reports/cucumber.json", 
 				  "pretty"},
-		features = {"src/test/java/com/ithr/ppe/test/cucumber/features"},
+//		features = {"src/test/java/com/ithr/ppe/test/cucumber/features"},
+				features = {"/Users/Marcus/Documents/ithr/features"},
 		tags = {"@netflixpurchase, @nowtvpurchase, @skypurchase, @spotifypurchase", "~@ignore"},
-//				tags = {"@checknet"},
 		glue = {"com.ithr.ppe.test.cucumber.steps"}
 		)
 public class CucumberRunner {
