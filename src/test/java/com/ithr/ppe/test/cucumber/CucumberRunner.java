a6ac29fe398e70fec4d6851902acce3ed37ee990
Marcus Catt <mc@marcuscatt.co.uk>
2016-08-30 17:56:49 +0100

    new offer [Issue6_text_errors]

--- src/test/java/com/ithr/ppe/test/cucumber/CucumberRunner.java
+++ src/test/java/com/ithr/ppe/test/cucumber/CucumberRunner.java
@@ -10,8 +10,8 @@ import cucumber.api.junit.Cucumber;
 				   "json:reports/cucumber.json", 
 				  "pretty"},
 		features = {"src/test/java/com/ithr/ppe/test/cucumber/features"},
-//		features = {"/Users/Marcus/Documents/ithr/features"},
-		tags = {"@netflixpurchase, @nowtvpurchase, @skypurchase, @spotifypurchase, @dropboxpurchase", "~@ignore"},
+//				features = {"/Users/Marcus/Documents/ithr/features"},
+		tags = {"@netflixpurchase, @nowtvpurchase, @skypurchase, @spotifypurchase", "~@ignore"},
 		glue = {"com.ithr.ppe.test.cucumber.steps"}
 		)
 public class CucumberRunner {
