3106cd2aad54a9008ff1d8fdd110dbc8e00a91e9
Marcus Catt <mc@marcuscatt.co.uk>
2016-09-14 10:25:57 +0100

    testing [Issue14_reporting]

---
+++ src/test/java/com/ithr/ppe/test/cucumber/MCExtentRunner.java
@@ -0,0 +1,38 @@
+package com.ithr.ppe.test.cucumber;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(	features = {"/Users/marcus/Documents/ithr/features"},
+					tags = {"@purchase, @checkit", "~@ignore"},
+					glue = {"com.ithr.ppe.test.cucumber.steps"},
+                    plugin = {"com.ithr.ppe.test.cucumber.ExtentCucumberFormatter"})
+
+
+public class MCExtentRunner {
+
+    @BeforeClass
+    public static void setup() {
+        ExtentCucumberFormatter.initiateExtentCucumberFormatter();
+        ExtentCucumberFormatter.loadConfig(new File("src/main/resources/extent-config.xml"));
+
+        ExtentCucumberFormatter.addSystemInfo("Browser Name", "Firefox");
+        ExtentCucumberFormatter.addSystemInfo("Browser version", "v47.0.1");
+        ExtentCucumberFormatter.addSystemInfo("Selenium version", "v2.53.1");
+
+        Map systemInfo = new HashMap();
+        systemInfo.put("Cucumber version", "v1.2.3");
+        systemInfo.put("Extent Cucumber Reporter version", "v1.1.0");
+        ExtentCucumberFormatter.addSystemInfo(systemInfo);
+    }
+
+}
