5b418147fa1d2a67f23d49537c77dba1b80627b9
Marcus Catt <mc@marcuscatt.co.uk>
2016-10-21 13:50:12 +0100

    corrected var declaration

--- pdfit.sh
+++ pdfit.sh
@@ -3,9 +3,9 @@
 hostname=`hostname`
 if [ "$hostname" == "SSS_Mac01" ]
 then
-        $ptowk="/usr/local/bin/wkhtmltopdf"
+        ptowk="/usr/local/bin/wkhtmltopdf"
 else
-        $ptowk="/bin/wkhtmltopdf"
+        ptowk="/bin/wkhtmltopdf"
 fi
 flist=""
 # improvment - path should be an env or param
@@ -18,6 +18,6 @@ flist2="anoverview.html failures-overview.html"
 date=`date +%Y%m%d_%H%M%S`
 cd $1 
 #st=`/usr/local/bin/wkhtmltopdf --javascript-delay 5000 $flist cucumer-report-${date}.pdf`
-st=`${ptowk} --javascript-delay 5000 $flist2 cucumer-report-${date}.pdf`
+st=`$ptowk --javascript-delay 5000 $flist2 cucumer-report-${date}.pdf`
 exit 0
 
