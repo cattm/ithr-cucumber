a7c76e1df6ba016692d97c69f8f06cdac822ea32
Marcus Catt <mc@marcuscatt.co.uk>
2016-10-24 10:44:37 +0100

    testing ITHR Jenkins with my new reporter

--- src/test/bin/pdfit.sh
+++ src/test/bin/pdfit.sh
@@ -18,6 +18,7 @@ flist2="anoverview.html failures-overview.html"
 date=`date +%Y%m%d_%H%M%S`
 cd $1 
 #st=`/usr/local/bin/wkhtmltopdf --javascript-delay 5000 $flist cucumer-report-${date}.pdf`
+echo "Command is: $ptowk --javascript-delay 5000 $flist2 cucumer-report-${date}.pdf"
 st=`$ptowk --javascript-delay 5000 $flist2 cucumer-report-${date}.pdf`
 exit 0
 
